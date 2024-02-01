d2f808d8bfe8ff953d738047c9ec59ffd2821ce0
Michel <michelbalvinor@gmail.com>
2024-02-01 17:52:10 -0500

    error 2

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,10 @@
 import express from "express";
 import cors from "cors";
-import path from "path";
 import { fileURLToPath } from 'url';
 import pg from "pg";
 import axios from 'axios';
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 import {
   DB_DATABASE,
