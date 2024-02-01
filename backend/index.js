66d9da48d7aa3a833d38e4d6374e940be0c9e818
Michel <michelbalvinor@gmail.com>
2024-01-31 22:54:52 -0500

    aa

--- backend/index.js
+++ backend/index.js
@@ -28,17 +28,17 @@ const pool = new pg.Pool({
 });
 
 // Servir archivos estáticos desde la carpeta 'build' (resultado de la construcción de React)
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'dist')));
 
 // Configuración de las rutas de tu aplicación React
 app.get("/privacypolicy", (req, res) => {
   // Responder con el contenido del archivo HTML principal de React
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
 app.get("/termsofservice", (req, res) => {
   // Responder con el contenido del archivo HTML principal de React
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
 // Otras rutas específicas de tu aplicación React si las tienes
