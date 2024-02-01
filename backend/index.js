1fa0c546eb0680c666219e1e68e32c67080c9829
Michel <michelbalvinor@gmail.com>
2024-02-01 18:28:30 -0500

    solu 3

--- backend/index.js
+++ backend/index.js
@@ -32,21 +32,26 @@ app.post('/getAccessToken', async (req, res) => {
   const { clientId, redirectUri, code } = req.body;
 
   try {
-    const response = await axios.post('https://api.instagram.com/oauth/access_token', {
-      client_id: clientId,
-      client_secret: '00a486c16ebc9243a199f671c0a7affe',
-      grant_type: 'authorization_code',
-      redirect_uri: redirectUri,
-      code: code,
+    const response = await axios.post('https://api.instagram.com/oauth/access_token', null, {
+      params: {
+        client_id: clientId,
+        client_secret: '00a486c16ebc9243a199f671c0a7affe',
+        grant_type: 'authorization_code',
+        redirect_uri: redirectUri,
+        code: code,
+      },
     });
 
     const responseData = response.data;
     res.json(responseData);
   } catch (error) {
     console.error('Error al obtener el token:', error);
-    res.status(500).json({ error: 'Error al obtener el token' });
+
+    // Enviar un mensaje de error específico al cliente
+    res.status(500).json({ error: 'Error al obtener el token', details: error.message });
   }
 });
+
 
 app.listen(PORT, () => {
   console.log(`Servidor iniciado en el puerto ${PORT}`);
