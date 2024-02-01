ea8bd705db220e9dadaefe15474486f8bcad1854
Michel <michelbalvinor@gmail.com>
2024-02-01 09:14:32 -0500

    prueba1

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -22,7 +22,7 @@ const Home = () => {
                 <NavLink className={(data) => console.log(data)} to={'/termsofservice'}> termsofservice</NavLink>
             </li>
         </ul>
-        <FacebookLoginButton appId="881471950379451"  onLogin={handleFacebookLogin}/>
+        <FacebookLoginButton appId="347868938167045"  onLogin={handleFacebookLogin}/>
 
     </div>
   );
