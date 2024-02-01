94c12d6691d527873bca934a32e238ba6a417f9a
Michel <michelbalvinor@gmail.com>
2024-02-01 17:28:19 -0500

    solution error 7

--- client/src/InstagramAuth.jsx
+++ client/src/InstagramAuth.jsx
@@ -1,47 +1,52 @@
 import React, { useEffect } from 'react';
-import axios from 'axios';
 
 const InstagramAuth = ({ onAuthorization }) => {
   const clientId = '751902293045624';
-  const clientSecret = '00a486c16ebc9243a199f671c0a7affe';
   const redirectUri = 'https://pruebaapifacebook.onrender.com/';
   const scope = 'user_profile,user_media';
   const responseType = 'code';
 
   useEffect(() => {
-    const handleWindowMessage = (event) => {
-      // Verificar que el mensaje proviene de la ventana de Instagram
-      if (event.origin === 'https://api.instagram.com') {
-        const { access_token, user_id } = event.data;
-        
-        // Ejecutar la función de retorno de llamada con el response
-        onAuthorization({ access_token, user_id });
-        
-        // Opcional: Cerrar la ventana emergente después de recibir el response
-        window.close();
+    const handleAuthCallback = async () => {
+      const urlParams = new URLSearchParams(window.location.search);
+      const code = urlParams.get('code');
+
+      if (code) {
+        try {
+          const response = await fetch('https://api.instagram.com/oauth/access_token', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            body: `client_id=${clientId}&client_secret=${encodeURIComponent(
+              'tu-client-secret'
+            )}&grant_type=authorization_code&redirect_uri=${encodeURIComponent(
+              redirectUri
+            )}&code=${code}`,
+          });
+
+          if (response.ok) {
+            const responseData = await response.json();
+
+            // Ejecutar la función de retorno de llamada con el response
+            onAuthorization(responseData);
+          } else {
+            console.error('Error al intercambiar el código por el token.');
+          }
+        } catch (error) {
+          console.error('Error en la solicitud de acceso:', error);
+        }
       }
     };
 
-    window.addEventListener('message', handleWindowMessage);
-
-    return () => {
-      window.removeEventListener('message', handleWindowMessage);
-    };
-  }, [onAuthorization]);
+    // Llamada a la función para manejar el código de autorización
+    handleAuthCallback();
+  }, [clientId, redirectUri, onAuthorization]);
 
   const handleAuthClick = () => {
     const authUrl = `https://api.instagram.com/oauth/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scope}&response_type=${responseType}`;
 
-    // Abrir la ventana emergente
-    const authWindow = window.open(authUrl, '_blank', 'width=600,height=600');
-
-    // Verificar si la ventana emergente se cerró
-    const checkWindowClosed = setInterval(() => {
-      if (authWindow.closed) {
-        clearInterval(checkWindowClosed);
-        // La ventana emergente se cerró, ejecutar alguna lógica adicional si es necesario
-      }
-    }, 1000);
+    window.location.href = authUrl;
   };
 
   return (
