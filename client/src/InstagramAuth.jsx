8cf87011be2cb0b5f768aaa1fadaa3ba75d5f649
Michel <michelbalvinor@gmail.com>
2024-02-02 15:15:27 -0500

    Mejorando 1.2

--- client/src/InstagramAuth.jsx
+++ client/src/InstagramAuth.jsx
@@ -7,60 +7,65 @@ const InstagramAuth = ({clientId, onAuthorization, btnText }) => {
   const scope = 'user_profile,user_media';
   const responseType = 'code';
 
-  const handleAuthClick = () => {
-    // Abrir la ventana emergente
+  const handleAuthClick = async () => {
     const authWindow = window.open(
       `https://api.instagram.com/oauth/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scope}&response_type=${responseType}`,
       '_blank',
       'width=600,height=600'
     );
-
-    // Verificar si la ventana se cerró
-    const checkWindowClosed = setInterval(async () => {
-      if (authWindow.closed) {
-        clearInterval(checkWindowClosed);
-        console.log('Ventana cerrada');
-      } else {
-        try {
-          // Intentar acceder al código desde la ventana emergente
-          const authCode = authWindow.location.href.split('code=')[1];
+  
+    const checkWindowClosed = setInterval(() => {
+      try {
+        if (authWindow.closed) {
+          clearInterval(checkWindowClosed);
+          console.log('Ventana cerrada');
+        } else {
+          const authCode = getAuthCodeFromUrl(authWindow.location.href);
           if (authCode) {
             clearInterval(checkWindowClosed);
             console.log('Código obtenido:', authCode);
-
-            // Realizar la solicitud al backend para obtener el token con Fetch y await
-            try {
-              const response = await fetch(`${URL}/getAccessToken`, {
-                method: 'POST',
-                headers: {
-                  'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({
-                  clientId,
-                  redirectUri,
-                  code: authCode,
-                }),
-                credentials: 'include', // Incluir el siguiente encabezado para enviar cookies y credenciales
-              });
-
-              const data = await response.json();
-
-              // Ejecutar la función de retorno de llamada con el response
-              onAuthorization(data);
-            } catch (error) {
-              console.error('Error al obtener el token:', error);
-            }
-
-            // Cerrar la ventana emergente después de obtener el código
             authWindow.close();
+            obtainAccessToken(authCode);
           }
-        } catch (error) {
-          // Si hay un error al intentar acceder al código, seguir verificando
         }
+      } catch (error) {
+        console.error('Error al verificar la ventana:', error);
       }
     }, 1000);
   };
-
+  
+  const getAuthCodeFromUrl = (url) => {
+    const match = url.match(/code=([^&]*)/);
+    return match && match[1];
+  };
+  
+  const obtainAccessToken = async (authCode) => {
+    try {
+      const response = await fetch(`${URL}/getAccessToken`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          clientId,
+          redirectUri,
+          code: authCode,
+        }),
+        credentials: 'include',
+      });
+  
+      if (response.ok) {
+        const data = await response.json();
+        onAuthorization(data);
+      } else {
+        console.error('Error al obtener el token:', response.statusText);
+      }
+    } catch (error) {
+      console.error('Error en la solicitud de token:', error);
+    }
+  };
+  
+    
   return (
     <> 
       <button onClick={handleAuthClick}>{btnText}</button>
